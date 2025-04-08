9570b011df5f869de74ad5b2adcb511af829809c
daanii03 <danielmarcosserrano760@gmail.com>
2025-04-08 15:56:19 +0200

    Solución Daniel MS

--- uBinaryTree.pas
+++ uBinaryTree.pas
@@ -148,65 +148,133 @@ end;
 { ---------------Ejercicio 1.1------------------ }
 procedure inorder_inverse(a: tbinaryTree);
 begin
-  writeln('NO IMPLEMENTADO');
+  if (a <> nil) then
+  begin
+       inorder_inverse(a^.hd);
+       visit(a^.info);
+       inorder_inverse(a^.hi);
+  end;
 end;
 
 { ---------------Ejercicio 1.2------------------ }
 function profundidad_max(a:tbinaryTree): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+     if a = nil then
+        profundidad_max:= 0
+     else
+       profundidad_max:= max(profundidad_max(a^.hi),profundidad_max(a^.hd))+1;
 end;
 
 { ---------------Ejercicio 1.3------------------ }
 function node_count(a:tbinaryTree): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+     if a = nil then
+        node_count:= 0
+     else
+         node_count:= 1 + node_count(a^.hi) + node_count(a^.hd);
 end;
 
 { ---------------Ejercicio 1.4------------------ }
 function leafs_count(a:tbinaryTree): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+     if a = nil then
+        leafs_count:= 0
+     else
+     begin
+          if (a^.hi = nil) and (a^.hd = nil) then
+             leafs_count:= 1+ leafs_count(a^.hd) + leafs_count(a^.hi)
+          else
+              leafs_count:= leafs_count(a^.hd) + leafs_count(a^.hi)
+     end;
 end;
 
 { ---------------Ejercicio 1.5------------------ }
 function internal_nodes_count(a:tbinaryTree): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+     if a = nil then
+        internal_nodes_count:= 0
+     else
+     begin
+          if (a^.hi <> nil) or (a^.hd <> nil) then
+             internal_nodes_count:= 1+ internal_nodes_count(a^.hi) + internal_nodes_count(a^.hd)
+          else
+              internal_nodes_count:= 0;
+     end;
 end;
 
 { ---------------Ejercicio 1.6------------------ }
 function is_full(a: tbinaryTree): boolean;
 begin
-  writeln('NO IMPLEMENTADO');
+
+     if ((a^.hd <> nil) and (a^.hi = nil)) or ((a^.hd = nil) and (a^.hi <> nil)) then
+           is_full:= False
+     else  if (a^.hd = nil) and (a^.hi = nil) then
+           is_full:= True
+     else
+           is_full:= is_full(a^.hd) and is_full(a^.hi);
+
 end;
 
   
   { ---------------Ejercicio 1.7------------------ }
 function max_hoja(a:tbinaryTree): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+     if a = nil then
+        max_hoja:= 0
+     else
+     begin
+          if  (a^.hd = nil) and (a^.hi = nil) then
+              max_hoja:= a^.info
+
+          else
+              max_hoja:= max(max_hoja(a^.hi),max_hoja(a^.hd));
+     end;
 end;
 
   
   { ---------------Ejercicio 1.8------------------ }
 function sum_hoja(a:tbinaryTree): integer; 
 begin
-  writeln('NO IMPLEMENTADO');
+     if a = nil then
+        sum_hoja:=0
+     else
+     begin
+          if (a^.hi = nil) and (a^.hd = nil) then
+             sum_hoja:=  a^.info
+          else
+              sum_hoja:= sum_hoja(a^.hi) + sum_hoja(a^.hd)
+     end;
 end;
 
 
   { ---------------Ejercicio 1.9------------------ }
 function num_pares(a:tbinaryTree): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+     if a = nil then
+        num_pares:= 0
+     else
+     begin
+          if a^.info mod 2 = 0 then
+             num_pares:= 1 + num_pares(a^.hi) + num_pares(a^.hd)
+          else
+              num_pares:= num_pares(a^.hi) + num_pares(a^.hd)
+     end;
 end;
 
   
     { ---------------Ejercicio 1.10------------------ }
 function num_nodos_en_nivel(a:tbinaryTree; nivel:integer): integer;
 begin
-  writeln('NO IMPLEMENTADO');
+     if a = nil then
+        num_nodos_en_nivel:= 0
+     else
+     begin
+          if nivel = 0 then
+             num_nodos_en_nivel:= 1
+          else
+              num_nodos_en_nivel:= num_nodos_en_nivel(a^.hi,nivel-1) + num_nodos_en_nivel(a^.hd,nivel-1) // Disminuyes el nivel para que llegue hasta 0 y devuelva 1 en cada recursión y estas se sumen
+
+     end;
 end;
 
 
